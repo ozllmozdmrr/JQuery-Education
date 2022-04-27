a70d13433561871fdf2e2afb6260e7422ab5eaee
ozllmozdmrr <ozllmozdmrr00@gmail.com>
2022-04-27 14:35:03 +0300

    Method

---
+++ app.js
@@ -0,0 +1,24 @@
+$(function(){
+    // $("p").children().css("color","red");
+   // $("p").children(":first").hide(); // span ile oluşturmuş olduğumuz ilk çocuk nesnesini silmiş olduk. 
+    //$("p").children(":last").hide(); // ikinci çocuk nesnesini sildik
+
+    //Parent
+    // $("span").parent().css("color","orange")
+
+    //Siblings()
+    // $("em").siblings().css("color","orange");
+
+    $("button").click(function(){
+        //$("h1").addClass("aktif");
+        // $("h1").removeClass("yeni");
+        $("h1").toggleClass("aktif");
+    });  
+
+
+
+
+
+
+
+});
